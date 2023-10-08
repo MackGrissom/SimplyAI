0bfdad20b9d00ede39b28d3b4c789b00f410dd4e
Mack Grissom <mcintyregrissom@gmail.com>
2023-10-08 11:22:22 +0700

    added coming soon animations

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -22,6 +22,8 @@ import { amountOptions, formSchema, resolutionOptions } from "./constants";
 
 import ImageBrain from "@/components/animation/image-brain.";
 import { ImageAnimation } from "@/components/ui/image-animation";
+import ComingSoon from "@/components/coming-soon";
+import animationData from '@/components/animation/comingsoon.json'; // Replace with your Lottie animation JSON file
 
 type FormValues = {
   prompt: string;
@@ -104,146 +106,149 @@ const PhotoPage = () => {
   }
 
   return (
-    <div className="text-white">
-      <Heading
-        title="Image Generation (Beta)"
-        description="Turn your prompt into an image."
-        icon={ImageIcon}
-        iconColor="text-pink-700"
-        bgColor="bg-pink-700/10"
-      />
-      <div className="px-4 lg:px-8">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="
-              rounded-lg 
-              border 
-              w-full 
-              p-4 
-              px-3 
-              md:px-6 
-              focus-within:shadow-sm
-              grid
-              grid-cols-12
-              gap-2
-            "
-          >
-            <FormField
-              name="prompt"
-              render={({ field }) => (
-                <FormItem className="col-span-12 lg:col-span-6">
-                  <FormControl className="m-0 p-0">
-                    <Input
-                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent text-black"
-                      disabled={isLoading}
-                      placeholder=" Ex: A 4k advertisement for a water bottle company in the swiss alps."
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem className="col-span-12 lg:col-span-2">
-                  <Select
-                    disabled={isLoading}
-                    onValueChange={field.onChange}
-                    value={field.value}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue defaultValue={field.value} />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {amountOptions.map((option) => (
-                        <SelectItem
-                          key={option.value}
-                          value={option.value}
-                        >
-                          {option.label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="resolution"
-              render={({ field }) => (
-                <FormItem className="col-span-12 lg:col-span-2">
-                  <Select
-                    disabled={isLoading}
-                    onValueChange={field.onChange}
-                    value={field.value}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue defaultValue={field.value} />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {resolutionOptions.map((option) => (
-                        <SelectItem
-                          key={option.value}
-                          value={option.value}
-                        >
-                          {option.label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormItem>
-              )}
-            />
+    // <div className="text-white">
+    //   <Heading
+    //     title="Image Generation (Beta)"
+    //     description="Turn your prompt into an image."
+    //     icon={ImageIcon}
+    //     iconColor="text-pink-700"
+    //     bgColor="bg-pink-700/10"
+    //   />
+    //   <div className="px-4 lg:px-8">
+    //     <Form {...form}>
+    //       <form
+    //         onSubmit={form.handleSubmit(onSubmit)}
+    //         className="
+    //           rounded-lg 
+    //           border 
+    //           w-full 
+    //           p-4 
+    //           px-3 
+    //           md:px-6 
+    //           focus-within:shadow-sm
+    //           grid
+    //           grid-cols-12
+    //           gap-2
+    //         "
+    //       >
+    //         <FormField
+    //           name="prompt"
+    //           render={({ field }) => (
+    //             <FormItem className="col-span-12 lg:col-span-6">
+    //               <FormControl className="m-0 p-0">
+    //                 <Input
+    //                   className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent text-black"
+    //                   disabled={isLoading}
+    //                   placeholder=" Ex: A 4k advertisement for a water bottle company in the swiss alps."
+    //                   {...field}
+    //                 />
+    //               </FormControl>
+    //             </FormItem>
+    //           )}
+    //         />
+    //         <FormField
+    //           control={form.control}
+    //           name="amount"
+    //           render={({ field }) => (
+    //             <FormItem className="col-span-12 lg:col-span-2">
+    //               <Select
+    //                 disabled={isLoading}
+    //                 onValueChange={field.onChange}
+    //                 value={field.value}
+    //                 defaultValue={field.value}
+    //               >
+    //                 <FormControl>
+    //                   <SelectTrigger>
+    //                     <SelectValue defaultValue={field.value} />
+    //                   </SelectTrigger>
+    //                 </FormControl>
+    //                 <SelectContent>
+    //                   {amountOptions.map((option) => (
+    //                     <SelectItem
+    //                       key={option.value}
+    //                       value={option.value}
+    //                     >
+    //                       {option.label}
+    //                     </SelectItem>
+    //                   ))}
+    //                 </SelectContent>
+    //               </Select>
+    //             </FormItem>
+    //           )}
+    //         />
+    //         <FormField
+    //           control={form.control}
+    //           name="resolution"
+    //           render={({ field }) => (
+    //             <FormItem className="col-span-12 lg:col-span-2">
+    //               <Select
+    //                 disabled={isLoading}
+    //                 onValueChange={field.onChange}
+    //                 value={field.value}
+    //                 defaultValue={field.value}
+    //               >
+    //                 <FormControl>
+    //                   <SelectTrigger>
+    //                     <SelectValue defaultValue={field.value} />
+    //                   </SelectTrigger>
+    //                 </FormControl>
+    //                 <SelectContent>
+    //                   {resolutionOptions.map((option) => (
+    //                     <SelectItem
+    //                       key={option.value}
+    //                       value={option.value}
+    //                     >
+    //                       {option.label}
+    //                     </SelectItem>
+    //                   ))}
+    //                 </SelectContent>
+    //               </Select>
+    //             </FormItem>
+    //           )}
+    //         />
         
 
 
 
 
-            <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
-              Generate
-            </Button>
-          </form>
-        </Form>
-        {isLoading && (
-          <div className="p-20">
-            <Loader />
-          </div>
-        )}
-        {photos.length === 0 && !isLoading && (
-        <ImageAnimation label='Please enter a prompt to generate an image..' />
+    //         <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
+    //           Generate
+    //         </Button>
+    //       </form>
+    //     </Form>
+    //     {isLoading && (
+    //       <div className="p-20">
+    //         <Loader />
+    //       </div>
+    //     )}
+    //     {photos.length === 0 && !isLoading && (
+    //     <ImageAnimation label='Please enter a prompt to generate an image..' />
           
-        )}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
-          {photos.map((src) => (
-            <Card key={src} className="rounded-lg overflow-hidden">
-              <div className="relative aspect-square">
-                <Image
-                  fill
-                  alt="Generated"
-                  src={src}
-                />
-              </div>
-              <CardFooter className="p-2">
-                <Button onClick={() => window.open(src)} variant="secondary" className="w-full">
-                  <Download className="h-4 w-4 mr-2" />
-                  Download
-                </Button>
-              </CardFooter>
-            </Card>
-          ))}
-        </div>
-      </div>
-    </div>
+    //     )}
+    //     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+    //       {photos.map((src) => (
+    //         <Card key={src} className="rounded-lg overflow-hidden">
+    //           <div className="relative aspect-square">
+    //             <Image
+    //               fill
+    //               alt="Generated"
+    //               src={src}
+    //             />
+    //           </div>
+    //           <CardFooter className="p-2">
+    //             <Button onClick={() => window.open(src)} variant="secondary" className="w-full">
+    //               <Download className="h-4 w-4 mr-2" />
+    //               Download
+    //             </Button>
+    //           </CardFooter>
+    //         </Card>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+    <ComingSoon animationData={animationData} text="Under Development. Coming Soon!"/>
+  </div>
   );
 }
 
